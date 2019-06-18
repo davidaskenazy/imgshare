0c2b3c6393495b925394b0d870620f9adf5ef9e3
davidaskenazy <davidask.da@gmail.com>
2019-06-18 17:27:52 -0300

    fix

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     database:{
-        URI: 'mongodb'+'srv://davidaskenazy:123456a@cluster0-syir3.mongodb.net/test?retryWrites=true&w=majority'
+        URI: 'mongodb://davidaskenazy:123456a@cluster0-shard-00-00-syir3.mongodb.net:27017,cluster0-shard-00-01-syir3.mongodb.net:27017,cluster0-shard-00-02-syir3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 
         
 
