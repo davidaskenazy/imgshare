21d61d480283763b00b815521291b48feb901612
davidaskenazy <davidask.da@gmail.com>
2019-06-18 16:35:11 -0300

    fix

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     database:{
-        URI: "mongodb+srv://davidaskenazy@gmail.com:W1941s1994!@cluster0-syir3.mongodb.net/test?retryWrites=true&w=majority" 
+        URI: "mongodb+srv://davidaskenazy:W1941s1994!@cluster0-syir3.mongodb.net/test?retryWrites=true&w=majority" 
 
         
 
