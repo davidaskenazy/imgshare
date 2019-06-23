40b8e56394934ab9235f0cae6aa05aae1329c6c6
davidaskenazy <davidask.da@gmail.com>
2019-06-23 12:32:39 -0300

    agregados

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -41,4 +41,40 @@ $(function() {
           
     }
   });
+
+
+  $('#btn-like').click(function(e) {
+    e.preventDefault();
+    let imgId = $(this).data('id');
+    console.log(imgId)
+    $.post('/images/' + imgId + '/like')
+      .done(data => {
+      console.log('back:', data)
+        $('.likes-count').text(data.likes);
+      });
+  });
+
+  // Delete Button comment
+  $('#btn-deleteC').click(function (e) {
+    e.preventDefault();
+    let $this = $(this);
+    const response = confirm('Are you sure you want to delete this comment?');
+    if (response) {
+      let imgIda = $(this).data('id');
+      $.ajax({
+        url: '/images/' + imgIda+'/comment',// aca no !
+        type: 'DELETE'
+      })
+        .done(function(result) {
+          $this.removeClass('btn-danger').addClass('btn-success');
+          $this.find('i').removeClass('fa-times').addClass('fa-check');
+          $this.append('');
+          var url = '/images/' + imgIda;
+          $(location).attr('href',url);
+          
+        });
+        //
+          
+    }
+  });
 });
